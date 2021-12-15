beb58c5d65ad9a5e87536ceba1b996a4acd36e68
unknown <c18301026@mytudublin.ie>
2021-12-15 07:51:05 +0000

    Added custom MIDI files & allowed for the playing/stopping of a MIDI file

--- MIDI Cinema/Assets/Scripts/GameController.cs
+++ MIDI Cinema/Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using MidiPlayerTK;
 
 public class GameController : MonoBehaviour
 {
@@ -9,10 +10,24 @@ public class GameController : MonoBehaviour
 	public float rotationSpeed = 100;
 	public float horizontalInput, verticalInput;
 
+	// Variables relating to the MPTK MIDI player
+	public MidiFilePlayer midiPlayer;
+	public bool midiIsPlaying = false;
+
 	// Start is called before the first frame update
 	void Start()
 	{
-		
+		// Set up the MIDI player
+		midiPlayer = FindObjectOfType<MidiFilePlayer>();
+
+		// You can change the MIDI file by replacing this string with a title in the MidiDB folder, e.g., "Bach - Fugue".
+		midiPlayer.MPTK_MidiName = "Heaven Can Wait";
+
+		// Create graphics when a MIDI event occurs.
+		midiPlayer.OnEventNotesMidi.AddListener(NotesToPlay);
+
+		// Turn the flag that indicates the MIDI is playing to false when the MIDI stops playing.
+		midiPlayer.OnEventEndPlayMidi.AddListener(EndPlay);
 	}
 
 	// Update is called once per frame
@@ -31,5 +46,30 @@ public class GameController : MonoBehaviour
 		{
 			transform.position = new Vector3(0, 30, -9);
 		}
+
+		// Press space to play the MIDI file
+		if(Input.GetKeyDown(KeyCode.Space) && midiIsPlaying == false)
+		{
+			midiPlayer.MPTK_Play();
+			midiIsPlaying = true;
+		}
+
+		// Press Q to stop playing the MIDI file
+		if(Input.GetKeyDown(KeyCode.Q))
+		{
+			midiPlayer.MPTK_Stop();
+		}
+	}
+
+	// Create graphics when a MIDI event occurs.
+	public void NotesToPlay(List<MPTKEvent> midiEvents)
+	{
+
+	}
+
+	// Turn the flag that indicates the MIDI is playing to false when the MIDI stops playing.
+	public void EndPlay(string name, EventEndMidiEnum reason)
+	{
+		midiIsPlaying = false;
 	}
 }
