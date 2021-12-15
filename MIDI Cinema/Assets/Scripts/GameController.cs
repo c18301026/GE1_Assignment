8e91fac5ba07005de205c941e4d4320c46107e12
unknown <c18301026@mytudublin.ie>
2021-12-15 07:15:31 +0000

    Respawn if you fall into the void

--- MIDI Cinema/Assets/Scripts/GameController.cs
+++ MIDI Cinema/Assets/Scripts/GameController.cs
@@ -25,5 +25,11 @@ public class GameController : MonoBehaviour
 		// The camera view can rotate horizontally or move back & forth
 		transform.Rotate(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);
 		transform.Translate(0, 0, verticalInput * speed * Time.deltaTime);
+
+		// Respawn if you fall into the void
+		if(transform.position.y < -50)
+		{
+			transform.position = new Vector3(0, 30, -9);
+		}
 	}
 }
