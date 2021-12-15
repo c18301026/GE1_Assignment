2e6a6606822b9a35cfcd4075d514a1386296ffab
unknown <c18301026@mytudublin.ie>
2021-12-15 08:16:39 +0000

    Created cube note render & NoteBehaviour.cs

---
+++ MIDI Cinema/Assets/Scripts/NoteBehaviour.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteBehaviour : MonoBehaviour
+{
+	public float speed = 15;
+
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Translate(-(speed * Time.deltaTime), 0, 0);
+
+		// Get rid of the note when it reaches the other side
+		if(transform.position.x < -26)
+		{
+			Destroy(gameObject);
+		}
+	}
+}
