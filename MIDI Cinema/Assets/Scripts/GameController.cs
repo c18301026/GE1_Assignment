bb2f0aa515c285abd33de3a540d5afc07c00fe07
unknown <c18301026@mytudublin.ie>
2021-12-15 07:01:18 +0000

    Added player control

---
+++ MIDI Cinema/Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	// Variables that control the player's movement
+	public float speed = 10;
+	public float rotationSpeed = 100;
+	public float horizontalInput, verticalInput;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		// Player input
+		horizontalInput = Input.GetAxis("Horizontal");
+		verticalInput = Input.GetAxis("Vertical");
+
+		// The camera view can rotate horizontally or move back & forth
+		transform.Rotate(0, horizontalInput * rotationSpeed * Time.deltaTime, 0);
+		transform.Translate(0, 0, verticalInput * speed * Time.deltaTime);
+	}
+}
